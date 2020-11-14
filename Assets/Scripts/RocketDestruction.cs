cb61779ab53b0752b74122d1ac854effd844ec9e
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-11-14 20:31:41 +0300

    Initial commit

---
+++ Assets/Scripts/RocketDestruction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketDestruction : MonoBehaviour
+{
+    public GameObject rockets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(rockets, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
